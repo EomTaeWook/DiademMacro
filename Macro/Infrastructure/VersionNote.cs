94e7a079ed3133b9e183930ec5d69607c8989c8d
EomTaeWook <trim0917@gmail.com>
2026-08-12 01:08:26 +0900

    버저닝

--- Macro/Infrastructure/VersionNote.cs
+++ Macro/Infrastructure/VersionNote.cs
@@ -5,8 +5,8 @@ namespace Macro.Infrastructure
     internal class Current
     {
         public static readonly int Major = 3;
-        public static readonly int Minor = 0;
-        public static readonly int Build = 4;
+        public static readonly int Minor = 1;
+        public static readonly int Build = 0;
     }
 
     public class VersionNote
